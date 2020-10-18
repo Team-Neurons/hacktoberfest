364aaa112623e79e7b877ef5800c94d05be27048
aflajismail <aflajahamed121@gmail.com>
2020-10-19 02:30:33 +0530

    modified function and css

--- Html/calculator/calculator.js
+++ Html/calculator/calculator.js
@@ -3,31 +3,40 @@ function calculate() {
   var oparator = document.getElementById("oparator").value;
   var num2 = document.getElementById("in2").value;
 
-  if (isNaN(num1) || isNaN(num2)) {
-    alert("Not a number");
-  } else if (oparator === "+") {
-    var sum = 0;
-    sum = parseInt(num1) + parseInt(num2);
-    document.getElementById("sum").innerHTML = "Summary=" + sum;
-  } else if (oparator === "-") {
-    var sum = 0;
-    sum = parseInt(num1) - parseInt(num2);
-    document.getElementById("sum").innerHTML = "Summary=" + sum;
-  } else if (oparator === "*") {
-    var sum = 0;
-    sum = parseInt(num1) * parseInt(num2);
-    document.getElementById("sum").innerHTML = "Summary=" + sum;
-  } else if (oparator === "/") {
-    var sum = 0;
-    sum = parseInt(num1) * parseInt(num2);
-    document.getElementById("sum").innerHTML = "Summary=" + sum;
-  } else if (oparator === "%") {
-    var sum = 0;
-    sum = parseInt(num1) * parseInt(num2);
-    document.getElementById("sum").innerHTML = "Summary=" + sum;
-  } else {
-    alert("Not a oparator format");
+  if (  num1 !="" && oparator != "" && num2 !="") {
+    
+    if (isNaN(num1) || isNaN(num2)) {
+      alert("Not a number");
+      } else if (oparator === "+") {
+      var sum = 0;
+      sum = parseInt(num1) + parseInt(num2);
+      document.getElementById("sum").innerHTML = "Summary=" + sum;
+      } else if (oparator === "-") {
+      var sum = 0;
+      sum = parseInt(num1) - parseInt(num2);
+      document.getElementById("sum").innerHTML = "Summary=" + sum;
+      } else if (oparator === "*") {
+      var sum = 0;
+      sum = parseInt(num1) * parseInt(num2);
+      document.getElementById("sum").innerHTML = "Summary=" + sum;
+      } else if (oparator === "/") {
+      var sum = 0;
+      sum = parseInt(num1) * parseInt(num2);
+      document.getElementById("sum").innerHTML = "Summary=" + sum;
+      } else if (oparator === "%") {
+      var sum = 0;
+      sum = parseInt(num1) * parseInt(num2);
+      document.getElementById("sum").innerHTML = "Summary=" + sum;
+      } else {
+      alert("Invalid Input");
+      }
+      document.getElementById("Equation").innerHTML =
+      "Equation=" + parseInt(num1) + oparator + parseInt(num2);
+
+  }else{
+    alert("Input Missing");
+  
   }
-  document.getElementById("pattern").innerHTML =
-    "Pattern=" + parseInt(num1) + oparator + parseInt(num2);
+     
+
 }
